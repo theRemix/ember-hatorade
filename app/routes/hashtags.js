d35e5a624de83d5802c5f80c9cf8d7416d978b11
voodoologic <headley.douglas@gmail.com>
2015-08-02 21:16:30 -0700

    infinite scroll, bitches.

--- app/routes/hashtags.js
+++ app/routes/hashtags.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import InfinityRoute from "ember-infinity/mixins/route";
 
-export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('hashtag')
+
+export default Ember.Route.extend(InfinityRoute, {
+  model() {
+    return this.infinityModel('hashtag',{ perPage: 50, startingPage: 1 });
   }
 });
--- app/routes/stream.js
+++ app/routes/stream.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
+import InfinityRoute from "ember-infinity/mixins/route";
 
-export default Ember.Route.extend({
-  model: function() {
-    return this.store.find('tweet');
+export default Ember.Route.extend(InfinityRoute,{
+  model() {
+    return this.infinityModel('tweet',{ perPage: 50, startingPage: 1 });
   },
   actions: {
     newTweet: function() {
       var fun = { tweet: { id: 420, text: "fun times", screen_name: "voodoologic", favorite_count: 99, url: "http://hatora.de", profile_image: "http://placekitten.com/90/90" } }
       console.log(this.currentModel)
       funtimes = this.store.createRecord('tweet', fun.tweet)
-      this.currentModel.pushObject(funtimes)
+      this.currentModel.unshiftObject(funtimes)
     }
   },
 });
