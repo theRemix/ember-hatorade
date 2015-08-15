c33507f043849e0a115bf6d5c9922347028a6e7e
voodoologic <headley.douglas@gmail.com>
2015-08-15 01:16:23 -0700

    update port

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   namespace: 'api/v1',
-  host: 'http://hatora.de'
+  host: 'http://api.hatora.de'
 });
--- sinatra_ember.rb
+++ sinatra_ember.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 
 configure do
+  set :port, 5055
   set :root, File.realpath(File.dirname(__FILE__))
   set :public_folder, File.expand_path('dist')
 end
