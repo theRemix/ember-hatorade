ecf57430110dc8e40bcd085f6380a76c25e8edc1
voodoologic <headley.douglas@gmail.com>
2015-11-26 11:24:59 -0700

    bootstrap tags, faye, modal

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -17,7 +17,10 @@ module.exports = function(defaults) {
   // If the library that you are including contains AMD or ES6
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  app.import('bower_components/faye-client/dist/faye-browser-min.js');
+  app.import('bower_components/bootstrap/js/modal.js');
+  app.import('bower_components/bootstrap-tagsinput/src/bootstrap-tagsinput.js');
+  app.import('bower_components/bootstrap-tagsinput/src/bootstrap-tagsinput.css');
 
   return app.toTree();
 };
