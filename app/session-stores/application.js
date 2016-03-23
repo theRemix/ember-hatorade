ae4bbdcf9eb944cb87822c4972ab3a9ead6ec35b
voodoologic <headley.douglas@gmail.com>
2016-03-23 16:22:51 -0700

    fix the dumb error

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -3,5 +3,5 @@ import CookieStore from 'ember-simple-auth/session-stores/cookie';
 export default CookieStore.extend({
   'cookieDomain': '.hatora.de',
   'cookieExpirationTime': 60 * 60 * 24
-}
+});
 
