ca3784f1e10fbfd4013e00be291ba6b2dc62ccd0
voodoologic <headley.douglas@gmail.com>
2018-01-10 19:41:01 -0800

    functional way to do modal

--- app/components/app-banner.js
+++ app/components/app-banner.js
@@ -1,4 +1,10 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  appTitle: 'HATORA.DE',
+  actions: {
+    toggleStreamModal() {
+      this.attrs.toggleStreamModal.update(true);
+    }
+  }
 });
